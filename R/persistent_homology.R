de42273370eab094651ff8a0fb8552aa23ba0830
Nello Blaser <nello.blaser@uib.no>
2017-01-26 17:50:20 +0100

    slight change in diameter calculation

--- R/persistent_homology.R
+++ R/persistent_homology.R
@@ -29,17 +29,18 @@ persistent_homology <- function(cover){
   pers <- as.data.frame(pers)
   colnames(pers) <- c("Dimension", "Birth", "Death")
   # minimum diameter
-  min_diameter <- cover@parameters$relative_diameter * cover@subsets[[1]]@diameter
+  max_diameter <- cover@subsets[[1]]@diameter
+  min_diameter <- cover@parameters$relative_diameter * max_diameter
   # add survivor
   pers <- rbind(pers, data.frame(Dimension = 0, 
                                  Birth = min_diameter, 
-                                 Death = cover@subsets[[1]]@diameter))
+                                 Death = max_diameter))
   # make values compatible with minimum diameter
   pers$Birth[pers$Birth < min_diameter] <- min_diameter
   pers$Death[pers$Death < min_diameter] <- min_diameter
   # remove diagonal
   pers <- pers[pers$Birth != pers$Death, ]
-  attr(pers, "maxdiam") <- cover@subsets[[1]]@diameter
+  attr(pers, "maxdiam") <- max_diameter
   attr(pers, "mindiam") <- min_diameter
   return(pers)
 }
