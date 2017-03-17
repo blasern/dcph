e6b502d6be395cfd89991286920646e21f2e837e
Nello Blaser <nello.blaser@uib.no>
2017-03-17 20:03:49 +0100

    fixed tests

--- tests/testthat/test-classes.R
+++ tests/testthat/test-classes.R
@@ -6,19 +6,19 @@ test_that("can create simple patches", {
 })
 
 test_that("can create simple covers", {
-  expect_true(is.cover(cover(distance_matrix = diag(1:3), subsets = list(patch(indices = 1:3)))))
+  expect_true(is.cover(cover(data = matrix(1:3, nrow = 3), subsets = list(patch(indices = 1:3)))))
 })
 
 test_that("cover dimension is correct", {
-  expect_equal(cover_dim(cover(distance_matrix = diag(1:3), 
+  expect_equal(cover_dim(cover(data = matrix(1:3, nrow = 3), 
                                subsets = list(patch(indices = 1:3)))), 1)
-  expect_equal(cover_dim(cover(distance_matrix = diag(1:3), 
+  expect_equal(cover_dim(cover(data = matrix(1:3, nrow = 3), 
                                subsets = list(patch(indices = 1:3), patch(indices = 1:3)))), 2)
 })
 
 test_that("covers are concatinated correctly", {
-  c1 <- cover(distance_matrix = diag(1:3), subsets = list(patch(indices = 1:3)))
-  c2 <- cover(distance_matrix = diag(1:3), subsets = list(patch(indices = 1:3), patch(indices = 1:3)))
+  c1 <- cover(data = matrix(1:3, nrow = 3), subsets = list(patch(indices = 1:3)))
+  c2 <- cover(data = matrix(1:3, nrow = 3), subsets = list(patch(indices = 1:3), patch(indices = 1:3)))
   expect_equal(c(c1, c1)@subsets, 
                c2@subsets)
 })
