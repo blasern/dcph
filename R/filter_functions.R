bb225de147e347c0ecd7e2acb513f80b8e14e850
Nello Blaser <nello.blaser@uib.no>
2017-03-21 09:57:29 +0100

    exporting filter functions

--- R/filter_functions.R
+++ R/filter_functions.R
@@ -1,3 +1,12 @@
+#' Filter functions
+#' 
+#' Filter after diameter or cardinality
+#' 
+#' @param patches Patch to apply filter
+#' @param data Data used in distance function
+#' @param distance_fct The used distance function
+#' @name filter_fct
+#' @export
 diameter_filter <- function(patches, data, distance_fct){
   sapply(patches, function(patch) diameter(patch, data, distance_fct))
 }
@@ -6,6 +15,8 @@ diameter <- function(patch, data, distance_fct){
   distance_fct(data, patch@anchor_points[1], patch@anchor_points[2])
 }
 
+#' @rdname
+#' @export
 cardinality_filter <- function(patches, ...){
   sapply(patches, function(x) length(x@indices))
 }
