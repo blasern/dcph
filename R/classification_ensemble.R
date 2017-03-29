d71ac27057fc6f46da74c7541a85fcfff61cbf9a
Nello Blaser <nello.blaser@uib.no>
2017-03-29 14:16:10 +0200

    added classification ensemble

---
+++ R/classification_ensemble.R
@@ -0,0 +1,78 @@
+#' Divisive prediction ensemble
+#' 
+#' A divisive prediction ensemble is an algorithm similar to random forests with
+#' divisive covers instead of decission trees. 
+#' 
+#' @param train Training data
+#' @param group Class of training data
+#' @param ntree Number of trees to grow
+#' @param depth Maximal depth of trees
+#' @param delta_range Range of delta parameters to consider
+#' @param dc_ensemble Output of \code{divisive_prediction_ensemble}
+#' @param test Test data
+#' 
+#' @examples
+#' # data
+#' sampled_index <- sample(150)
+#' train <- iris[sampled_index[1:100], 1:4]
+#' group <- iris[sampled_index[1:100], 5]
+#' test <- iris[sampled_index[101:150], 1:4]
+#' observed <- iris[sampled_index[101:150], 5]
+#' 
+#' # fit divisive prediction ensemble
+#' fit <- divisive_prediction_ensemble(train, group, ntree = 10, depth = 10, delta_range = c(0, 0))
+#' # get predictions
+#' predictions <- predict_divisive_prediction_ensemble(fit, test)
+#' 
+#' # calculate misclassification error
+#' table(observed, predictions)
+#' tab <- table(observed, predictions)
+#' misclassification_error <- 1 - sum(diag(tab)) / sum(tab)
+#' misclassification_error
+#' @export
+divisive_prediction_ensemble <- function(train, group, ntree = 100, depth = 1000, delta_range = c(0, 0.2)){
+  replicate(ntree, random_division_prediction(train, group, depth, delta_range), simplify = FALSE)
+}
+
+random_division_prediction <- function(train, group, depth, delta_range){
+  # sample data, features and relative gap
+  rows <- sample(nrow(train), replace = TRUE)
+  features <- sample(1:ncol(train), sqrt(ncol(train)))
+  relative_gap <- runif(1, delta_range[1], delta_range[2])
+  
+  dc <- divisive_cover(data = train[rows, features], 
+                       group = group[rows], 
+                       distance_fct = distance_cdist("euclidean"), 
+                       stop_fct = stop_relative_filter_max_nodes(relative_filter = 0, max_nodes = depth), 
+                       anchor_fct = anchor_classify, 
+                       filter_fct = classification_filter, 
+                       division_fct = relative_gap_division(relative_gap = relative_gap))
+
+  return(list(features = features, rows = rows, relative_gap = relative_gap, dc = dc))
+}
+
+#' @rdname divisive_prediction_ensemble
+#' @export
+predict_divisive_prediction_ensemble <- function(dc_ensemble, test){
+  individual_predictions <- lapply(dc_ensemble, predict_dc_probabilities, test = test)
+  # predictions
+  predictions <- sapply(individual_predictions, identity)
+  # voting
+  majority_voting <- apply(predictions, 1, function(x){
+    uniqv <- unique(x)
+    tab <- tabulate(match(x, uniqv))
+    uniqv[which.max(tab)]
+  })
+  
+  # return
+  majority_voting
+}
+
+predict_dc_probabilities <- function(dc_list, test){
+  features <- dc_list$features
+  relative_gap <- dc_list$relative_gap
+  dc <- dc_list$dc
+  pc <- predict(object = dc, newdata = test[, features], 
+                predict_fct = relative_gap_prediction(relative_gap = dc_list$relative_gap))
+  dc_pred <- group_from_predict_cover(pc)
+}
