cd006e31ec30204ef63d586af1665e7f7b0b12b7
Nello Blaser <nello.blaser@uib.no>
2017-04-10 09:23:37 +0200

    documentation for user-defined division functions

--- R/division_functions.R
+++ R/division_functions.R
@@ -11,6 +11,10 @@
 #' The function \code{relative_gap_division} uses the \code{relative_factor_division}
 #' with \code{relative_factor = (1-2 * relative_gap)/(1+2 * relative_gap)}
 #' 
+#' Note that all division functions should return a list of two patches, where
+#' the first patch contains the parent anchor point 1 and the second patch 
+#' contains parent anchor point 2. 
+#' 
 #' @param relative_factor corresponds to (1-2 * delta)/(1+2 * delta)
 #' @param relative_gap delta-parameter for delta-filtered cover (0 < delta <= 1/2)
 #' @param euclidean logical, if TRUE then (1-delta)/(1+delta) is used instead of 
